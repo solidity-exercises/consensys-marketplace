996a5fa728938ffbd511b0e3680a7d6f08a142e1
ArnaudovSt <telerikacademy2016@gmail.com>
2018-08-20 23:05:28 +0300

    retrying coverage

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,3 @@
 module.exports = {
-	testrpcOptions: '-p 8555 -l 0xfffffffffff -g 0x01 -e 4200 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"',
+	testrpcOptions: '-p 8555 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"',
 };
