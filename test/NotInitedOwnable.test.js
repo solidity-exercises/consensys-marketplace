dafab04e53c82a37d508b16132d505084a941bff
ArnaudovSt <telerikacademy2016@gmail.com>
2018-08-21 02:10:01 +0300

    added tests for NotInitedOwnable

---
+++ test/NotInitedOwnable.test.js
@@ -0,0 +1,78 @@
+const NotInitedOwnable = artifacts.require('NotInitedOwnable');
+
+const assertRevert = require('./utils/test.util').assertRevert;
+const inLogs = require('./utils/test.util').inLogs;
+
+contract('NotInitedOwnable', function ([coinbase, another]) {
+	let sut;
+	const ZERO_ADDRESS = '0x' + '0'.repeat(40);
+
+	beforeEach(async function () {
+		sut = await NotInitedOwnable.new();
+	});
+
+	it('Should not have owner When instantiated', async function () {
+		// Arrange
+		// Act
+		const owner = await sut.owner.call();
+		// Assert
+		assert.equal(owner, ZERO_ADDRESS);
+	});
+
+	it('init Should set owner When called', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const owner = await sut.owner.call();
+		// Assert
+		assert.equal(owner, coinbase);
+	});
+
+	it('init Should revert When called with already set owner', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const result = sut.init();
+		// Assert
+		await assertRevert(result);
+	});
+
+	it('transferOwnership Should transfer the ownership When passed valid arguments', async function () {
+		// Arrange
+		await sut.init();
+		const initialOwner = await sut.owner.call();
+		assert.equal(initialOwner, coinbase);
+		// Act
+		await sut.transferOwnership(another);
+		const owner = await sut.owner.call();
+		// Assert
+		assert.equal(owner, another);
+	});
+
+	it('transferOwnership Should raise OwnershipTransferred event When passed valid arguments', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const { logs } = await sut.transferOwnership(another);
+		// Assert
+		await inLogs(logs, "OwnershipTransferred", { previousOwner: coinbase, newOwner: another });
+	});
+
+	it('transferOwnership Should revert When the passed `_newOwner` account equals 0', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const result = sut.transferOwnership(ZERO_ADDRESS);
+		// Assert
+		await assertRevert(result);
+	});
+
+	it('transferOwnership Should revert When called from non-owner account', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const result = sut.transferOwnership(another, { from: another });
+		// Assert
+		await assertRevert(result);
+	});
+});
--- test/OwnableUpgradeableProxy.test.js
+++ test/OwnableUpgradeableProxy.test.js
@@ -21,18 +21,27 @@ contract('UpgradeableProxy', function ([coinbase, another]) {
 		});
 
 		it("should be able to call getter method of the first contract", async function () {
+			// Arrange
+			// Act
 			const num = await implementedContract.getNum.call();
+			// Assert
 			assert(num.eq(1000), "The getNum did not return correctly");
 		});
 
 		it("should be able to call setter method of the first contract", async function () {
+			// Arrange
 			await implementedContract.setRate(43);
+			// Act
 			const rate = await implementedContract.rate.call();
+			// Assert
 			assert(rate.eq(43), "The rate did not return correctly");
 		});
 
 		it("should set the owner of the first contract", async function () {
+			// Arrange
+			// Act
 			const owner = await implementedContract.owner.call();
+			// Assert
 			assert.strictEqual(coinbase, owner, "Owner not set correctly");
 		});
 	});
@@ -47,26 +56,35 @@ contract('UpgradeableProxy', function ([coinbase, another]) {
 		});
 
 		it("should be able to upgrade contract", async function () {
+			// Arrange
 			const firstNum = await implementedContract.getNum.call();
 			const upgradeableContract = await IUpgradeableImplementation.at(proxy.address);
+			// Act
 			await upgradeableContract.upgradeImplementation(impl2.address);
 			const secondNum = await implementedContract.getNum.call();
+			// Assert
 			assert(firstNum.eq(1000), "The firstNum did not return correctly");
 			assert(secondNum.eq(2000), "The secondNum did not return correctly");
 		});
 
 		it("should be able to read from the upgraded contract", async function () {
+			// Arrange
 			await implementedContract.setRate(43);
 			const rate = await implementedContract.rate.call();
+			// Act
 			assert(rate.eq(43), "The first rate was not set correctly");
 			const upgradeableContract = await IUpgradeableImplementation.at(proxy.address);
 			await upgradeableContract.upgradeImplementation(impl2.address);
 			const rate2 = await implementedContract.rate.call();
+			// Assert
 			assert(rate2.eq(43), "The second rate was not set correctly");
 		});
 
 		it("should throw on upgrade contract from not owner", async function () {
+			// Arrange
+			// Act
 			const upgradeableContract = await IUpgradeableImplementation.at(proxy.address);
+			// Assert
 			await assertRevert(upgradeableContract.upgradeImplementation(impl2.address, { from: another }));
 		});
 	});
