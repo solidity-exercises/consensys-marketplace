aa8af235f52dfe28b52f5d4223ab0a0f5825379f
ArnaudovSt <telerikacademy2016@gmail.com>
2018-08-26 16:27:54 +0300

    added deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,14 @@
 const StoreManager = artifacts.require('./StoreManager.sol');
-const StoreFactory = artifacts.require('./StoreFactory.sol');
+const UpgradeableProxy = artifacts.require('./UpgradeableProxy.sol');
+const Aggregated = artifacts.require('./Aggregated.sol');
 
 module.exports = function (deployer, ...args) {
-	deployer.deploy(StoreFactory);
-	deployer.link(StoreFactory, StoreManager);
-	deployer.deploy(StoreManager);
+	deployer.then(async () => {
+		await deployer.deploy(StoreManager);
+		const marketplaceImplementation = await StoreManager.deployed();
+		await deployer.deploy(UpgradeableProxy, marketplaceImplementation.address);
+		const proxy = await UpgradeableProxy.deployed();
+		const upgradeableMarketplace = await Aggregated.at(proxy.address);
+		await upgradeableMarketplace.init();
+	});
 };
