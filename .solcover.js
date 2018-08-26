8d8680ff2ae986c499b2a61390ac09c6f49bd399
ArnaudovSt <telerikacademy2016@gmail.com>
2018-08-26 22:02:06 +0300

    another try on solidity coverage

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,3 @@
 module.exports = {
-	testrpcOptions: '--allowUnlimitedContractSize -p 8555 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"',
-	skipFiles: ['abstractions/']
+	testrpcOptions: '--allowUnlimitedContractSize -p 8555 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"'
 };
