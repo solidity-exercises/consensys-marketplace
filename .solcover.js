20970289ad8faf2e60b3bac878d4e4098a504690
ArnaudovSt <telerikacademy2016@gmail.com>
2018-08-26 21:43:34 +0300

    leaving on solidity coverage

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,4 @@
 module.exports = {
 	testrpcOptions: '--allowUnlimitedContractSize -p 8555 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"',
-	testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-	compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
 	skipFiles: ['abstractions/']
 };
