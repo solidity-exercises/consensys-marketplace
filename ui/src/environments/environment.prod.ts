2d25a46ec4e5717740dce8d8dc671712e3340a6d
ArnaudovSt <telerikacademy2016@gmail.com>
2018-08-28 12:33:33 +0300

    deployed on Rinkeby

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
 	production: true,
 	provider: 'http://localhost:8545',
 	Abi: marketplaceAbi['marketplace'],
-	address: '' // TODO: Put the address from rinkeby deployment
+	address: '0xb42d3214eeC65D3E6A6257a778823aD093cbD7Fd'
 };
